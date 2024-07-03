6dae48fea03a0c8b90025b864da9f0f9817f32ed
egv83 <esteban8305@hotmail.com>
2024-07-02 22:03:34 -0500

    evm - funciones como parametros

--- arrowFunctions/funciones.js
+++ arrowFunctions/funciones.js
@@ -1,26 +1,56 @@
-sumar = function(){
+
+// sumar = function(a,b){
+//     let resultado = v1+v2;
+//     alert("El resultado es: "+resultado);
+// }
+
+ejecutarSumar = () => {
+
     let caja1 = document.getElementById("txtValor1");
     let caja2 = document.getElementById("txtValor2");
-
 
     let v1 = parseInt(caja1.value);
     let v2 = parseInt(caja2.value);
 
-    let resultado = v1+v2;
-
-    alert("El resultado es: "+resultado);
-}
+    sumar(v1, v2);
+};
 
 /* SINTAXIS ARROW FUNCTION*/
-restar = () =>{
+sumar = (a, b) => {
+    let resultado = a + b;
+    alert("El resultado es: " + resultado);
+};
+
+ejecutarRestar = () => {
     let caja1 = document.getElementById("txtValor1");
     let caja2 = document.getElementById("txtValor2");
 
+    let v1 = parseInt(caja1.value);
+    let v2 = parseInt(caja2.value);
+    restar(v1, v2);
+};
+
+restar = (a, b) => {
+    let resultado = a - b;
+    alert("El resultado es: " + resultado);
+};
+
+buscarMenor = (x, y) => {
+    if (x < y) {
+        alert("El menor es: "+x);
+    }else{
+        alert("El menor es: "+y);
+    }
+}
+
+/* FUNCIONES COMO PARAMETROS*/
+ejecutar = (fnOperacion) => {
+    let caja1 = document.getElementById("txtValor1");
+    let caja2 = document.getElementById("txtValor2");
 
     let v1 = parseInt(caja1.value);
     let v2 = parseInt(caja2.value);
 
-    let resultado = v1-v2;
+    fnOperacion(v1, v2);
 
-    alert("El resultado es: "+resultado);
 }
